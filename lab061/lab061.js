985d94ad2cd03d31916db2930d76f4cead0d84c7
Noah Leigh <noah.d.leigh@gmail.com>
2014-04-09 11:34:23 -0400

    Initial Import of Current Madlib Project
    Includes the lab06 page and the first version of it.

---
+++ lab061/lab061.js
@@ -0,0 +1,18 @@
+/**************************************
+ TITLE: Hands On 17: Pulling Through
+ AUTHOR: Noah Leigh
+ CREATE DATE: 2014-04-07
+ PURPOSE: Pulling Text From a Text Field
+ LAST MODIFIED ON: 2014-04-07
+ LAST MODIFIED BY: Noah Leigh
+ MODIFICATION HISTORY: Created
+*/
+
+
+$(document).ready(function(){
+    $("#input").submit(function(){
+        console.log("Submit!"); // Debug Message
+        $("#output").text($("#input1").val());  // Scape and assign in one line.
+        return false;   // Don't refresh the page.
+    });
+});
